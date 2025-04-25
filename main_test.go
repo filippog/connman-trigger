b47451b51d6a6c2c4242cde362f9fde28388b48e
Filippo Giunchedi <filippo@debian.org>
2025-04-25 21:30:38 +0200

    Test dbus interactions

--- main_test.go
+++ main_test.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/godbus/dbus/v5"
 )
 
 // TestIsExecutable verifies that isExecutable correctly identifies executable files
@@ -29,6 +31,159 @@ func TestIsExecutable(t *testing.T) {
 
 	if !isExecutable(name) {
 		t.Errorf("expected executable file to return true, got false")
+	}
+}
+
+// Fake implementations for testing getNetworkDetailsForService
+// fakeBusObject implements dbus.BusObject for tests
+type fakeBusObject struct {
+	call *dbus.Call
+}
+
+func (fbo *fakeBusObject) Call(method string, flags dbus.Flags, args ...interface{}) *dbus.Call {
+	return fbo.call
+}
+
+// CallWithContext satisfies dbus.BusObject
+func (fbo *fakeBusObject) CallWithContext(ctx context.Context, method string, flags dbus.Flags, args ...interface{}) *dbus.Call {
+	return fbo.call
+}
+
+// Go satisfies dbus.BusObject
+func (fbo *fakeBusObject) Go(method string, flags dbus.Flags, ch chan *dbus.Call, args ...interface{}) *dbus.Call {
+	return fbo.call
+}
+
+// GoWithContext satisfies dbus.BusObject
+func (fbo *fakeBusObject) GoWithContext(ctx context.Context, method string, flags dbus.Flags, ch chan *dbus.Call, args ...interface{}) *dbus.Call {
+	return fbo.call
+}
+
+// AddMatchSignal satisfies dbus.BusObject
+func (fbo *fakeBusObject) AddMatchSignal(iface, member string, options ...dbus.MatchOption) *dbus.Call {
+	return fbo.call
+}
+
+// RemoveMatchSignal satisfies dbus.BusObject
+func (fbo *fakeBusObject) RemoveMatchSignal(iface, member string, options ...dbus.MatchOption) *dbus.Call {
+	return fbo.call
+}
+
+// GetProperty satisfies dbus.BusObject
+func (fbo *fakeBusObject) GetProperty(p string) (dbus.Variant, error) {
+	return dbus.MakeVariant(nil), nil
+}
+
+// StoreProperty satisfies dbus.BusObject
+func (fbo *fakeBusObject) StoreProperty(p string, value interface{}) error {
+	return nil
+}
+
+// SetProperty satisfies dbus.BusObject
+func (fbo *fakeBusObject) SetProperty(p string, v interface{}) error {
+	return nil
+}
+
+// Destination satisfies dbus.BusObject
+func (fbo *fakeBusObject) Destination() string {
+	return ""
+}
+
+// Path satisfies dbus.BusObject
+func (fbo *fakeBusObject) Path() dbus.ObjectPath {
+	return ""
+}
+
+// fakeConn implements dbusConnection
+type fakeConn struct {
+	obj dbus.BusObject
+}
+
+func (fc fakeConn) Object(dest string, path dbus.ObjectPath) dbus.BusObject {
+	return fc.obj
+}
+
+func TestGetNetworkDetailsForService(t *testing.T) {
+	cases := []struct {
+		name     string
+		call     *dbus.Call
+		wantName string
+		wantType string
+	}{
+		{
+			name: "GoodProperties",
+			call: &dbus.Call{
+				Body: []interface{}{map[string]dbus.Variant{
+					"Name": dbus.MakeVariant("myssid"),
+					"Type": dbus.MakeVariant("wifi"),
+				}},
+				Err: nil,
+			},
+			wantName: "myssid",
+			wantType: "wifi",
+		},
+		{
+			name: "CallError",
+			call: &dbus.Call{
+				Body: []interface{}{map[string]dbus.Variant{
+					"Name": dbus.MakeVariant("name"),
+					"Type": dbus.MakeVariant("type"),
+				}},
+				Err: errors.New("fail"),
+			},
+			wantName: "unknown",
+			wantType: "unknown",
+		},
+		{
+			name: "EmptyBody",
+			call: &dbus.Call{
+				Body: []interface{}{},
+				Err:  nil,
+			},
+			wantName: "unknown",
+			wantType: "unknown",
+		},
+		{
+			name: "BadBodyType",
+			call: &dbus.Call{
+				Body: []interface{}{"notamap"},
+				Err:  nil,
+			},
+			wantName: "unknown",
+			wantType: "unknown",
+		},
+		{
+			name: "MissingName",
+			call: &dbus.Call{
+				Body: []interface{}{map[string]dbus.Variant{
+					"Type": dbus.MakeVariant("ethernet"),
+				}},
+				Err: nil,
+			},
+			wantName: "unknown",
+			wantType: "unknown",
+		},
+		{
+			name: "MissingType",
+			call: &dbus.Call{
+				Body: []interface{}{map[string]dbus.Variant{
+					"Name": dbus.MakeVariant("ssidonly"),
+				}},
+				Err: nil,
+			},
+			wantName: "unknown",
+			wantType: "unknown",
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			fbo := &fakeBusObject{call: tc.call}
+			conn := fakeConn{obj: fbo}
+			name, typ := getNetworkDetailsForService(conn, dbus.ObjectPath("/test"))
+			if name != tc.wantName || typ != tc.wantType {
+				t.Errorf("%s: got (%q, %q), want (%q, %q)", tc.name, name, typ, tc.wantName, tc.wantType)
+			}
+		})
 	}
 }
 
